9d1f86a5ba5cc0fa3789a0c1fe005dcb98a24b26
Mubarizullah <mubarizullah@crosscode.dev>
2024-10-09 18:35:55 +0500

    perfect Movement Around Corners too

--- Kitchen_Chaos/Assets/Scripts/Player.cs
+++ Kitchen_Chaos/Assets/Scripts/Player.cs
@@ -14,11 +14,31 @@ public class Player : MonoBehaviour
    {
     Vector2 inputVector2FromGameInput = gameInput.GetMovementVector2Normalized();
     Vector3 movDir = new Vector3(inputVector2FromGameInput.x, 0, inputVector2FromGameInput.y);
+    Vector3 movDirX = new Vector3(movDir.x, 0, 0);
+    Vector3 movDirZ = new Vector3(0, 0, movDir.z);
     
     Vector3 endPoint = transform.position + Vector3.up * bodyHeight;                            //position of this gameobject is its base while we move up from base 2 units to reach its height
     bool canMove = !Physics.CapsuleCast(transform.position, endPoint, bodyRadius, movDir, playerSpeed * Time.deltaTime, layerMaskForCounters);
     
-    
+    if (!canMove)       //mean we have hit something lets check if we can move on x axis for this we will direction dedicated for x-axis
+    {
+      canMove = !Physics.CapsuleCast(transform.position, endPoint, bodyRadius, movDirX, playerSpeed* Time.deltaTime, layerMaskForCounters);
+
+      if (canMove)
+      {
+        movDir = movDirX;
+      }
+      else
+      {
+        canMove = !Physics.CapsuleCast(transform.position, endPoint, bodyRadius, movDirZ, playerSpeed * Time.deltaTime, layerMaskForCounters);
+        if (canMove)
+        {
+          movDir = movDirZ;
+        }
+      }
+
+
+    }
 
 
     if (canMove)
