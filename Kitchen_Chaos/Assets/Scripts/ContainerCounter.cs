b7d18ff6722f74638d53ae3db0b793b9adaa8c5a
Admin <admin@crosscode.dev>
2024-10-24 18:58:05 +0500

    3:55:22 applying container counter script to game object

---
+++ Kitchen_Chaos/Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContainerCounter : MonoBehaviour, IKitchenObjectParent
+{
+
+    private KitchenObjects kitchenObjects;
+    [SerializeField] KitchenObjectSO kitchenObjectSO;
+    [SerializeField] Transform spawnPointCounter;
+
+
+    public void Interact(Player player)
+    {
+        if (kitchenObjects == null)
+        {
+            Transform kitchenObjTransform = Instantiate(kitchenObjectSO.prefab, spawnPointCounter);
+            kitchenObjTransform.GetComponent<KitchenObjects>().SetKitchenObjectParent(this);
+        }
+        else
+        {
+            // clear counter already has kitchen object now return this to player
+            kitchenObjects.SetKitchenObjectParent(player);
+        }
+    }
+
+
+
+
+    public Transform GetKitchenObjSpawnPoint()
+    {
+        return spawnPointCounter;
+    }
+
+
+
+    public void SetKitchenObject(KitchenObjects kitchenObjects)
+    {
+        this.kitchenObjects = kitchenObjects;
+    }
+
+
+
+    public KitchenObjects GetKitchenObjects()
+    {
+        return kitchenObjects;
+    }
+
+
+
+    public bool IsKitchenObjectPresent()
+    {
+        return kitchenObjects != null;
+    }
+
+
+
+    public void ClearKitchenObject()
+    {
+        kitchenObjects = null;
+    }
+}
