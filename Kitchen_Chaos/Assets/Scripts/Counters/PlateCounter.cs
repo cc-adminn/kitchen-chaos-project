1654010d3d684edc9b47ab247ef0a5c2e7433bff
Admin <admin@crosscode.dev>
2024-11-05 18:56:55 +0500

    6:05:28 plates spawing automatically

---
+++ Kitchen_Chaos/Assets/Scripts/Counters/PlateCounter.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System;
+
+public class PlateCounter : BaseCounter
+{
+    
+    public event EventHandler OnPlateSpawned;
+
+    float plateSpawnTime;
+    float plateSpawnTimeMax = 4f;
+    int plateCount;
+    int plateCountMax = 4;
+
+    private void Update()
+    {
+        plateSpawnTime += Time.deltaTime;
+        if (plateSpawnTime > plateSpawnTimeMax)
+        {
+            plateSpawnTime = 0;
+            if (plateCount < plateCountMax)
+            {
+                plateCount++;
+                OnPlateSpawned?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
---
+++ Kitchen_Chaos/Assets/Scripts/Counters/PlateCounterVisuals.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class PlateCounterVisuals : MonoBehaviour
+{
+    [SerializeField] PlateCounter plateCounter;
+    [SerializeField] Transform plateVisuals;
+    [SerializeField] Transform counterTopPoint;
+    private List<GameObject> plateSpawnedList;
+
+    private void Awake()
+    {
+        plateSpawnedList = new List<GameObject>();
+    }
+    private void Start()
+    {
+        plateCounter.OnPlateSpawned += PlateCounter_OnPlateSpawned;
+    }
+
+    private void PlateCounter_OnPlateSpawned(object sender, System.EventArgs e)
+    {
+        Transform plateVisual = Instantiate(plateVisuals, counterTopPoint);
+        Debug.Log(plateVisuals.name);
+        float plateOffSet = .1f;
+        plateVisual.localPosition = new Vector3(0, plateOffSet * plateSpawnedList.Count);
+
+        plateSpawnedList.Add(plateVisual.gameObject);
+    }
+}
