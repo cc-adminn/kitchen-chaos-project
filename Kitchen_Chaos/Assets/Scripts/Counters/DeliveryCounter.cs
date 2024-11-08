7143816bc3f63013d0d469601c65a63dfeecc14f
Admin <admin@crosscode.dev>
2024-11-08 18:27:30 +0500

    Shader Graph of Delivery Counter

---
+++ Kitchen_Chaos/Assets/Scripts/Counters/DeliveryCounter.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class DeliveryCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (player.IsKitchenObjectPresent())
+        {
+            if (player.GetKitchenObjects().TryGetPlateOBject(out PlateKitchenObject plateKitchenObject))
+            {
+                player.GetKitchenObjects().DestroyItself();
+            }
+        }
+        
+    }
+}
