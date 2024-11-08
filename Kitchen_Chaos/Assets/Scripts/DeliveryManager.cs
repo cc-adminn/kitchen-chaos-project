1595dee74e0a6979696fe4dea8476701e5768dae
Admin <admin@crosscode.dev>
2024-11-08 18:57:46 +0500

    7:11:20

---
+++ Kitchen_Chaos/Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+
+public class DeliveryManager : MonoBehaviour
+{
+    private List<RecipeSO> waitingRecipeSO;
+    [SerializeField] RecipeListSO recipeListSO;
+}
